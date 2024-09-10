795f565095f494289e36cd6d8f1390666661dcfb
Lauren Rebecca DeMaria <demarialr@s.dcsdk12.org>
2024-09-10 12:26:47 -0600

    First commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,126 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Video 1_11',
+    'What are the main building blocks of Flutter?',
+    [
+      'Widgets'
+      'Functions'
+      'Blocks'
+      'Components'
+    ]
+  ),
+  QuizQuestion(
+    'Video 2_2',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),
+  QuizQuestion(
+    'Video 2_3',
+    'How is Dart code compiled?',
+    [
+      'The code is parsed by Dart and translated into a language that the device knows, and then executed'
+      'The code is copied onto the device and executed'
+      'Dart parses the code and feeds that into the device'
+      'Dart code is compiled as is'
+    ]
+  ),
+  QuizQuestion(
+    'Video 2_6',
+    'What do dependencies do?',
+    [
+      'Manage all third party packages'
+      'Manage functions'
+      'Manage widgets'
+      'Automatically format the code'
+    ]
+  ),
+  QuizQuestion(
+    'Video 2_7',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),QuizQuestion(
+    'Video 2_12',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),QuizQuestion(
+    'Video 2_18',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),QuizQuestion(
+    'Video 2_22',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),QuizQuestion(
+    'Video 2_23',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),QuizQuestion(
+    'Video 2_27',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),QuizQuestion(
+    'Video 2_32',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),QuizQuestion(
+    'Video 3_5',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),QuizQuestion(
+    'Video 3_10',
+    'Where is code stored and run?',
+    [
+      'Lib'
+      'Build'
+      'Test'
+      'Android'
+    ]
+  ),
+];
---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,7 @@
+class QuizQuestion {
+  const QuizQuestion(this.video, this.text, this.answers);
+  
+  final String video;
+  final String text;
+  final List<String> answers;
+}
